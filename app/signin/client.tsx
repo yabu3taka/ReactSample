6f4567be08d2922b77cf69efbb4414ee98039812
yabushita <yabu3taka@gmail.com>
2025-05-18 21:47:05 +0900

    ログイン失敗を表示

--- app/signin/client.tsx
+++ app/signin/client.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import { useState } from 'react';
 import { useForm, SubmitHandler } from 'react-hook-form';
 import { signInHandler } from './server';
 
@@ -7,16 +8,28 @@ export function SignInForm() {
     // 毎回入力するのが面倒なのでデフォルトで設定。
     const defaultValues = { email: "user1@example.com", password: "passwd1" };
     const methods = useForm({ defaultValues });
-    const { handleSubmit } = methods;
+    const { handleSubmit, register, formState: { errors } } = methods;
+
+    const [error, setError] = useState("");
+
 
     const internalOnCommit: SubmitHandler<typeof defaultValues> = async (formData) => {
-        await signInHandler(formData);
+        try {
+            await signInHandler(formData);
+        } catch (e) {
+            setError("ログイン失敗");
+        }
     }
 
     return (
         <form onSubmit={handleSubmit(internalOnCommit)}>
-            <input type="text" name="email" placeholder="Email" defaultValue={defaultValues.email} /><br />
-            <input type="text" name="password" defaultValue={defaultValues.password} /><br />
+            {error}<br />
+            <input type="text" placeholder="Email" defaultValue={defaultValues.email} {...register("email", { required: true })} />
+            {errors.email?.type === 'required' && <span>入力必要</span>}
+            <br />
+            <input type="text" defaultValue={defaultValues.password} {...register("password", { required: true })} />
+            {errors.password?.type === 'required' && <span>入力必要</span>}
+            <br />
             <button type="submit">ログイン</button>
         </form>
     )
